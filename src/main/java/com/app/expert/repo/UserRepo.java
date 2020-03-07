09d634278af1449d1e59b5610b7a7cf647e82e36
Ranel <rickitacki@gmail.com>
2020-03-07 15:59:49 +0300

    Revert "Initial commit"
    This reverts commit e0cc1530d3a41ba5cad15b7c7f6aba8dd775d78f.

--- src/main/java/com/app/expert/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.expert.repo;
-
-
-import com.app.expert.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
